ce1b054ed1eaa8f7d04b4481c0ae3151ed867e95
Gina Beth Russell <ginabethrussell@Ginas-MBP.cinci.rr.com>
2020-11-13 14:01:35 -0500

    added buttons to each class to push to signup page on click

--- src/components/SampleClasses.js
+++ src/components/SampleClasses.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import {useHistory} from 'react-router-dom';
 import axios from 'axios';
 import styled from 'styled-components';
 
@@ -77,7 +78,7 @@ const PageTitle = styled.h1`
     color: #ffffff;
     font-weight: 600;
     line-height: 1.65;
-    border-bottom: 2px solid white;
+    border-bottom: 2px solid #ffffff;
     margin-bottom: 20px;
     @media(max-width: 670px){
         width: 70%;
@@ -86,30 +87,81 @@ const PageTitle = styled.h1`
         margin-bottom: 0;
     }`;
 const ClassDisplayDiv = styled.div`
-    width: 90%;
-    margin: 0 auto;
+    width: 100%;
     display: flex;
-    flex-wrap: wrap;
-    justify-content: space-evenly;
-    align-content: space-around;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
     `;
 const ClassCard = styled.div`
-    margin-bottom: 20px;
-    width: 30%;
+    width: 90%;   
+    margin: 0 auto; 
+    display: flex;
+    align-items:center;
+    justify-content: space-between;
     color: #ffffff;
-    margin: 20px;
     @media(max-width: 850px){
-        width: 50%;
-        margin-bottom: 0;
+        flex-direction: column;
     };
-    @media(max-width: 670px){
-        width: 75%;
-    }
 `;
+const ImageDiv = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 40%;
+    padding: 20px;
+    @media(max-width: 850px){
+        width: 80%;
+        order: -99;
+    };
+    `;
+
+const InfoDiv = styled.div`
+    width: 50%;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    text-align: left;
+    @media(max-width: 850px){
+        width: 80%;
+    };
+    `;
+
+const ClassTitle = styled.h3`
+    font-family: Source Sans Pro, sans-serif;
+    font-size: 2.2rem;
+    font-weight: 700;
+    text-transform: uppercase;
+    margin-bottom: 0;
+    padding-bottom: 5px;
+    border-bottom: 2px solid #ffffff;
+     `;
+
+const ClassDescription = styled.p`
+    font-family: Source Sans Pro, sans-serif;
+    font-size: 2rem;
+    `;
+
+const Button = styled.button`
+    border: 1px solid #ffffff;
+    background-color: transparent;
+    padding: 16px 30px;
+    text-transform: uppercase;
+    font-size: 1.6rem;
+    color: #ffffff;
+    &:hover{
+        border-color: #9bf1ff;
+        color: #9bf1ff;
+    }
+    @media(max-width: 850px){
+        padding: 12px 20px;
+    };
+    `;
 
 export default function SampleClasses() {
     const [classes, setClasses] = useState([]);
-
+    const history = useHistory();
     // Set up axios get request to get classlist object from server
     // useEffect(()=> {
     //     axios.get('/sampleclasses')
@@ -133,13 +185,37 @@ export default function SampleClasses() {
             <ClassDisplayDiv >
                 {classes.map(sampleClass => (
                 <ClassCard key={sampleClass.id}>
-                    <h3>{sampleClass.name}</h3>
-                    <img
-                    src={sampleClass.imageUrl}
-                    alt={sampleClass.name}
-                    width="100%"
-                    />
-                    <p>{sampleClass.description}</p>
+                { !!(Number(sampleClass.id) % 2) ?
+                   (<>
+                    <ImageDiv>
+                        <img
+                        src={sampleClass.imageUrl}
+                        alt={sampleClass.name}
+                        width="100%"
+                        />
+                    </ImageDiv>
+                    <InfoDiv>
+                        <ClassTitle>{sampleClass.name}</ClassTitle>
+                        <ClassDescription>{sampleClass.description}</ClassDescription>
+                        <Button onClick={()=> history.pushState('/signup')}>Get Started</Button>
+                    </InfoDiv>
+                    </>
+                   ): 
+                   (<>
+                    <InfoDiv>
+                        <ClassTitle>{sampleClass.name}</ClassTitle>
+                        <ClassDescription>{sampleClass.description}</ClassDescription>
+                        <Button onClick={()=> history.push('/signup')}>Get Started</Button>
+                    </InfoDiv>
+                    <ImageDiv>
+                        <img
+                        src={sampleClass.imageUrl}
+                        alt={sampleClass.name}
+                        width="100%"
+                        />
+                    </ImageDiv>
+                    </>)
+                }
                 </ClassCard>
                 ))}
                 </ClassDisplayDiv>
