162bcadeb58b979b442fafb0740aa0f18d583e0a
Sathya Ganesan <sathyaganesan.b2w@gmail.com>
2020-11-13 21:57:41 -0500

    Post request is created for adding activity

--- src/InstructorComponents/InstructorPage.js
+++ src/InstructorComponents/InstructorPage.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { ActivitiesList } from './ActivitiesList';
+import { ActivityDetails} from './ActivityDetails';
 
 
 export const InstructorPage = () => {
@@ -16,92 +18,22 @@ export const InstructorPage = () => {
         }
     ]);
 
-    const submitHandler = (e) => {
-        e.preventDefault();
-    }
-
-    const changeHandler = (e) => {
-        e.persist();
-    }
-
+    useEffect(() => { 
+        axios
+            .get(` https://anywhere-fitness-tt-webpt-88.herokuapp.com/classes`)
+            .then((res) => {
+                console.log("GET REQUEST", res.data);
+                SetActivities(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+        })
+    }, []);
 
     return (
         <div>
-            <p>Instructor Page</p>
-            <form>
-                <div onClick = {submitHandler}>
-                    <input
-                        type = "text"
-                        name="name"
-                        placeholder="Name"
-                        value={activities.name}
-                        onChange = {changeHandler}
-                    />
-                </div>
-                <div>
-                    <input
-                        type="text"
-                        name="type"
-                        placeholder="Type"
-                        value={activities.type}
-                        onChange = {changeHandler}
-                    />
-                </div>
-                <div>
-                    <input
-                        type="text"
-                        name="time"
-                        placeholder="Time"
-                        value={activities.time}
-                        onChange = {changeHandler}
-                    />
-                </div>
-                <div>
-                    <input
-                        type="text"
-                        name="duration"
-                        placeholder="Duration"
-                        value={activities.duration}
-                        onChange = {changeHandler}
-                    />
-                </div>
-                <div>
-                    <input
-                        type="text"
-                        name="intensity"
-                        placeholder="Intensity"
-                        value={activities.intensity}
-                        onChange = {changeHandler}
-                    />
-                </div>
-                <div>
-                    <input
-                        type="text"
-                        name="location"
-                        placeholder="location"
-                        value={activities.location}
-                        onChange = {changeHandler}
-                    />
-                </div>
-                <div>
-                    <input
-                        type="text"
-                        name="numberOfRegisteredAttendees"
-                        placeholder="Number Of Registered Attendees"
-                        value={activities.numberOfRegisteredAttendees}
-                        onChange = {changeHandler}
-                    />
-                </div>
-                <div>
-                    <input
-                        type="text"
-                        name="maxClassSize"
-                        placeholder="Max Class Size"
-                        value={activities.maxClassSize}
-                        onChange = {changeHandler}
-                    />
-                </div>
-            </form>
+            <ActivitiesList activities={activities} updateActivity={SetActivities}/>
+            <ActivityDetails activities={activities}/>
         </div>
         
     )
