a659eda647639d3fa4f4dcbcd866909f95a9418c
Sathya Ganesan <sathyaganesan.b2w@gmail.com>
2020-11-16 15:24:10 -0500

    Done with css for Instructor page

--- src/InstructorComponents/ActivityDetails.js
+++ src/InstructorComponents/ActivityDetails.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export const ActivityDetails = ({activities}) => {
 
+    const [cart, setCart] = useState([]);
+
+    const addToCart = (e) => {
+        setCart({ ...cart, e });
+        console.log("CART", e);
+    }
 
     return (
         <div className = "activity_details">
             <h2> Activity Details</h2>
             {activities.map((item) => (
-                <div className = "activity">
-                    <h3>{item.name}</h3>
-                    <p>{ item.type}</p>
-                    <p>{ item.date}</p>
-                    <p>{ item.duration}</p>
-                    <p>{ item.intensity}</p>
-                    <p>{ item.location}</p>
+                <div className="activity">
+                    <div>
+                        <h3>{item.name}</h3>
+                        <p>{ item.type}</p>
+                        <p>{ item.date}</p>
+                        <p>{ item.duration}</p>
+                        <p>{ item.intensity}</p>
+                        <p>{item.location}</p>
+                    </div>
+                    <div>
+                        <button onClick = {() => addToCart(item)}>Enroll</button>
+                    </div>
                 </div>
+                
             ))}
         </div>
     );
